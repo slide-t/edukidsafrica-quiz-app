39ad9968fd992d2ac3417b5d0310f6e78ac86419
slide-t <slideteckacademy@gmail.com>
2025-06-23 14:56:32 +0100

    Update script.js

--- BST/level1/script.js
+++ BST/level1/script.js
@@ -351,7 +351,7 @@ function showResult() {
 }
 
 function goToNextLevel() {
-  window.location.href = "https://slide-t.github.io/edukidsafrica-quiz-app/English/level2/index.html";
+  window.location.href = "https://slide-t.github.io/edukidsafrica-quiz-app/BST/level2/index.html";
 }
 
 // Only run when the DOM is fully loaded
